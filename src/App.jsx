05f0cf56399895a96b013b7c1cbad91ec63a8232
omar <omarqais225@gmail.com>
2025-03-09 00:43:15 +0200

    creating HowItWorks section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
+import HowItWorks from './components/HowItWorks';
 const App = () => {
   return (
     <main className='text-sm text-neutral-300 antialiased'>
       <Navbar />
       <HeroSection />
+      <HowItWorks />
     </main>
   );
 }
