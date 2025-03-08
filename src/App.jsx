db77b994e4e5bd97be392dbffed5a672a248e561
omar <omarqais225@gmail.com>
2025-03-09 01:41:49 +0200

    creating keyfeatures section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import HowItWorks from './components/HowItWorks';
+import KeyFeatures from './components/KeyFeatures';
 const App = () => {
   return (
     <main className='text-sm text-neutral-300 antialiased'>
       <Navbar />
       <HeroSection />
       <HowItWorks />
+      <KeyFeatures />
     </main>
   );
 }
