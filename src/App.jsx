2ca217ead625cacac7e4136f5d6bec21c2b173a7
omar <omarqais225@gmail.com>
2025-03-09 02:31:54 +0200

    creating footer section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HowItWorks from './components/HowItWorks';
 import KeyFeatures from './components/KeyFeatures';
 import PricingPlans from './components/PricingPlans';
 import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
 const App = () => {
   return (
     <main className='text-sm text-neutral-300 antialiased'>
@@ -14,6 +15,7 @@ const App = () => {
       <KeyFeatures />
       <PricingPlans />
       <Testimonials />
+      <Footer />
     </main>
   );
 }
